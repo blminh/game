5c4ec3deac12d56dfbd7ff603d2ab06e20dc22c2
blminh <43545452+blminh@users.noreply.github.com>
2019-05-26 11:47:43 +0700

    Add files via upload

---
+++ h/file_in_out.h
@@ -0,0 +1,10 @@
+#ifndef __FILE_IN_OUT__
+#define __FILE_IN_OUT__
+#include "linked_list.h"
+
+void read_file(linked_list &list);
+void Introduce();
+void file_high_score(int high_score, int &flag);
+void highScore();
+
+#endif
